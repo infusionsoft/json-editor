7aadc493e100482349533d6c8332ed081d1e2a7d
Brian Adamich <badamich@gmail.com>
2017-05-12 11:09:06 -0700

    prevent remove_empty_property from deleting boolean values of false

--- src/editors/object.js
+++ src/editors/object.js
@@ -672,7 +672,7 @@ JSONEditor.defaults.editors.object = JSONEditor.AbstractEditor.extend({
     if(this.jsoneditor.options.remove_empty_properties || this.options.remove_empty_properties) {
       for(var i in result) {
         if(result.hasOwnProperty(i)) {
-          if(!result[i]) delete result[i];
+          if(!result[i] && result[i].toString() != "false") delete result[i];
         }
       }
     }
